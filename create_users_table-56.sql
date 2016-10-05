9fd40bc90181d76993b7e606dae2b16abc8834ca
Stéphane Bourzeix <stephane@bourzeix.com>
2016-10-05 09:11:18 +0200

    Added 2 different SQL script for Mysql 5.0/5.5 and >=5.6

--- create_users_table-56.sql
+++ create_users_table-56.sql
@@ -1,13 +1,13 @@
 # Sample table structure for the SimpleLoginSecure CodeIgniter Library.
-# Version for MySQL 5.0 or 5.5
+# Version for MySQL 5.6 and more
 
 CREATE TABLE `users` (
   `user_id` int(10) unsigned NOT NULL auto_increment,
   `user_email` varchar(255) NOT NULL default '',
   `user_pass` varchar(60) NOT NULL default '',
-  `user_date` datetime NOT NULL default '0000-00-00 00:00:00',
-  `user_modified` datetime NOT NULL default '0000-00-00 00:00:00',
-  `user_last_login` datetime NULL default NULL,
+  `user_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_last_login` datetime DEFAULT NULL,
   PRIMARY KEY  (`user_id`),
   UNIQUE KEY `user_email` (`user_email`)
 ) DEFAULT CHARSET=utf8;
